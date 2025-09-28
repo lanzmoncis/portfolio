b718a7af607bea8b16a7766875f9c45474a5fd3d
lanzmoncis <lanzmoncis@gmail.com>
2025-09-28 20:33:23 +0100

    Add news project

--- components/home/projects.tsx
+++ components/home/projects.tsx
@@ -6,7 +6,7 @@ import ArrowRight from "@/public/assets/arrow-right.svg";
 export const Projects = () => {
   return (
     <section className="space-y-8 pb-16">
-      <h3 className="text-2xl">Latest Projects</h3>
+      <h3 className="text-2xl">Projects</h3>
       <ul className="w-full space-y-2">
         <li className="px-3 py-4 -mx-3 hover:text-gray-400 duration-300">
           <div className="flex justify-between items-center">
@@ -70,6 +70,43 @@ export const Projects = () => {
               <div>
                 <Link
                   href={"https://github.com/lanzmoncis/ecommerce"}
+                  target="_blank"
+                >
+                  <div className="uppercase hover:text-gray-100 duration-300 flex gap-3 items-center">
+                    <Image
+                      src={ArrowRight}
+                      alt="arrow top on square"
+                      className="hidden md:block"
+                    />
+                    <span>view code</span>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li className="px-3 py-4 -mx-3 hover:text-gray-400 duration-300">
+          <div className="flex justify-between items-center">
+            <div className="space-y-4 md:space-y-2">
+              <h3 className="text-lg">News</h3>
+              <p>News web app</p>
+            </div>
+            <div className="space-y-6 md:space-y-3 text-sm text-inherit">
+              <div>
+                <Link href={"https://ncnews-lanz.netlify.app/"} target="_blank">
+                  <div className="uppercase hover:text-gray-100 duration-300 flex gap-3 items-center">
+                    <Image
+                      src={ArrowRight}
+                      alt="arrow top on square"
+                      className="hidden md:block"
+                    />
+                    <span>Live site</span>
+                  </div>
+                </Link>
+              </div>
+              <div>
+                <Link
+                  href={"https://github.com/lanzmoncis/news-fe"}
                   target="_blank"
                 >
                   <div className="uppercase hover:text-gray-100 duration-300 flex gap-3 items-center">
